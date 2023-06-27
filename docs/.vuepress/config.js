38edc22c1afddc4247c05e554c83fc281def95c3
zhoulei <zhoulei@qq.com>
2023-06-27 14:51:01 +0800

    feat: 增加百度数据统计功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,44 @@ module.exports = {
   description: "记录分享学习笔记",
   theme: "reco",
   base: "/my-blog/",
-  plugins: ['@vuepress/last-updated'],
+  plugins: [
+    //   显示最后更新时间
+    [
+      "@vuepress/last-updated",
+      {
+        transformer: (timestamp, lang) => {
+          const moment = require("moment");
+          moment.locale(lang);
+          return moment(timestamp).fromNow();
+        },
+      },
+    ],
+  ],
   locales: {
     "/": {
       lang: "zh-CN",
     },
   },
+  head: [
+    // 增加百度统计
+    [
+      "script",
+      {},
+      `
+       var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5372c5fa5d06a8e2ebc558cbc6908bc9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+        </script>        
+        `,
+    ],
+  ],
   themeConfig: {
     subSidebar: "auto",
-    lastUpdated: '上次更新',
+    lastUpdated: "上次更新",
     nav: [
       { text: "首页", link: "/" },
       {
@@ -25,7 +54,7 @@ module.exports = {
         title: "欢迎学习",
         path: "/",
         collapsable: false, // 不折叠
-        children:[]
+        children: [],
       },
       {
         title: "基础学习",
---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,12 @@
+export default ({ router }) => {
+    router.beforeEach((to, from, next) => {
+        if (typeof _hmt !== "undefined") {
+            if (to.path) {
+                // 增加百度统计
+                _hmt.push(["_trackPageview", to.fullPath]);
+            }
+        }
+
+        next();
+    });
+};
